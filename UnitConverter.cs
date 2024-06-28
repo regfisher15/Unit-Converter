70ee514ca2675f531e460a58860a1e64aaaec8cd
rbfisher15 <rbfisher@crimson.ua.edu>
2024-06-27 23:48:09 -0500

    Add options for the user

--- UnitConverter.cs
+++ UnitConverter.cs
@@ -2,6 +2,24 @@ using System;
 
 class UnitConverter {
     static void Main() {
-        Console.WriteLine("Hello World!");
+        Console.WriteLine("Welcome to the Unit Converter!");
+        Console.WriteLine("Choose a number from the menu below:");
+
+        //blank line
+        Console.WriteLine();
+
+        //the options
+        Console.WriteLine("1: Meters to feet");
+        Console.WriteLine("2: Kilograms to Pounds");
+        Console.WriteLine("3: Feet to Meters");
+        Console.WriteLine("4: Pounds to Kilograms");
+        Console.WriteLine("5: Celsius to Fahrenheit");
+        Console.WriteLine("6: Gallons to Liters");
+
+        //user input (also convert from string to int)
+        //int choice = int.Parse(Console.ReadLine());
+
+        
+
     }
 }
